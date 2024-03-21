d8620bc6a578e053282029d491e630de4d8fcebc
hk <kiptooharon.hk@gmail.com>
2024-03-20 20:59:25 -0400

    removed name column

--- models/user.py
+++ models/user.py
@@ -10,7 +10,6 @@ class User(BaseModel, Base):
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    name = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
     places = relationship("Place", back_populates="user",
