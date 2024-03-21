386e58053789a7d8c635a875b1eefff43b95f9bc
hk <kiptooharon.hk@gmail.com>
2024-03-20 20:53:47 -0400

    removed cant be null constraint on firstname, lastname column of users

--- models/user.py
+++ models/user.py
@@ -10,8 +10,8 @@ class User(BaseModel, Base):
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(18), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
     places = relationship("Place", back_populates="user",
                           cascade="all, delete-orphan")
     reviews = relationship("Review", back_populates="user",
