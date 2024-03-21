f74bfd34ec7cad6cf671a507a7de2d854cfdd3a7
hk <kiptooharon.hk@gmail.com>
2024-03-20 21:49:34 -0400

    updated sql script to create test database

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,8 +1,6 @@
 -- test database setup
 -- USEr setup
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY "hbnb_test_pwd";
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-USE hbnb_dev_db;
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-USE mysql;
 GRANT SELECT ON perfomance_schema.* TO 'hbnb_test'@'localhost';
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
