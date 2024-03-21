397519b69cd079ed2d5909e5b7f8f7da286b3a5f
hk <kiptooharon.hk@gmail.com>
2024-03-20 20:56:54 -0400

    added name column and increased password character len field

--- models/user.py
+++ models/user.py
@@ -9,7 +9,8 @@ class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
-    password = Column(String(18), nullable=False)
+    password = Column(String(128), nullable=False)
+    name = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
     places = relationship("Place", back_populates="user",
