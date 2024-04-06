2887519e3dde6addf693b8757a04f403ab8c976b
kiptoo haron <kiptooharon.hk@gmail.com>
2024-04-06 14:40:59 -0400

    pack for deployment

---
+++ 1-pack_web_static.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+    pack web static web pages for upload
+    to  the server
+"""
+
+from os import path, mkdir
+from datetime import datetime
+from fabric import task
+from invoke import run
+
+
+@task
+def do_pack(c):
+
+    """Create a tar gzipped archive of the directory web_static."""
+    now = datetime.utcnow().strftime("%Y%M%D%H%M%S").replace('/', '')
+    tarball = "web_static_{}.tgz".format(now)
+    if path.isdir("versions") is False:
+        run("mkdir -p versions")
+    else:
+        tar_path = "versions" + "/" + tarball
+        print("Packing web_static to {}\n".format(tar_path))
+        if run("tar -cvzf {} web_static".format(tar_path)).exited == 0:
+            print("\nDone.")
+            return tar_path
+    return None
