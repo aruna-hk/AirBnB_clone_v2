e14ea17baa24bc8ba0dce68abe92e4ba90d994c4
kiptoo haron <kiptooharon.hk@gmail.com>
2024-04-07 09:52:18 -0400

    pack web pages for deployment

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
-""" prepares files for deployment """
-import os.path
+"""
+    pack web static web pages for upload
+    to  the server
+"""
+
+from os import path, mkdir
 from datetime import datetime
-from fabric.api import local
+from fabric import task
+from invoke import run
 
 
-def do_pack():
-    """creating archive to deploy"""
+@task
+def do_pack(c):
 
-    date = datetime.utcnow()
-    now = str(date.year) + str(date.mont) + str(date.day) + str(date.hour) + \
-            str(date.minute) + str(date.second)
-    file = "versions/web_static_{}.tgz".format(now)
-    if os.path.isdir("versions") is False:
-        if local("mkdir -p versions").failed is True:
-            return None
-    if local("tar -cvzf {} web_static".format(file)).failed is True:
-        return None
-    return file
+    """Create a tar gzipped archive of the directory web_static."""
+    now = datetime.utcnow().strftime("%Y%M%D%H%M%S").replace('/', '')
+    tarball = "web_static_{}.tgz".format(now)
+    if path.isdir("versions") is False:
+        run("mkdir -p versions")
+    tar_path = "versions" + "/" + tarball
+    print("Packing web_static to {}\n".format(tar_path))
+    if run("tar -cvzf {} web_static".format(tar_path)).exited == 0:
+        print("\nDone.")
+        return tar_path
+
+    return None
