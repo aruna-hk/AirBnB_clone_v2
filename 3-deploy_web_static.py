1c79cd52c265b49ef8a45b7242ea338e7ca8e62e
kiptoo haron <kiptooharon.hk@gmail.com>
2024-04-14 14:30:50 +0300

    deploying same archive to servers

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -4,14 +4,24 @@
    FULL DEPLOYMENT
    create archive and distribute to the servers
 """
-from os import path, mkdir
-from fabric.api import local
+from fabric.api import local, env, run, put
+from os import path
 from datetime import datetime
-from fabric.api import run, put, env
 
-env.hosts = ['web-01', 'web-02']
-env.user = "ubuntu"
-env.key_filename = "/home/x230/.ssh/school"
+
+def do_pack():
+    """
+        pack web pages to .tgz file for upload
+    """
+    now = datetime.utcnow().strftime("%Y%m%d%H%M%S").replace('/', '')
+
+    if path.isdir("versions") is False:
+        local("rm -f versions")
+        mkdir("versions")
+    version = "versions/web_static_" + now + ".tgz"
+    if (local("tar -cvzf {} web_static".format(version)).succeeded):
+        return version
+    return None
 
 
 def do_deploy(archive_path):
@@ -27,7 +37,7 @@ def do_deploy(archive_path):
             archive_path.split('.')[0].split('/')[1] + "/"
         run("mkdir -p {}".format(release_dir))
         run("tar -xzf {} -C {}".format(remote, release_dir))
-        run("rm {}".format(remote))
+        run("rm -rf {}".format(remote))
         run("rm -rf /data/web_static/current")
         run("mv {}web_static/* {}".format(release_dir, release_dir))
         run("rm -rf {}web_static".format(release_dir))
@@ -37,25 +47,18 @@ def do_deploy(archive_path):
     return False
 
 
-def do_pack():
-    """
-        pack web pages to .tgz file for upload
-    """
-    now = datetime.utcnow().strftime("%Y%M%D%H%M%S").replace('/', '')
-
-    if path.isdir("versions") is False:
-        mkdir("versions")
-    version = "versions/web_static_" + now + ".tgz"
-    if (local("tar -cvzf {} web_static".format(version)).succeeded):
-        return version
-    return None
-
-
 def deploy():
     """ full deployment"""
 
     tarball = do_pack()
     if (tarball is None):
         return False
-    server_status = do_deploy(tarball)
-    return server_status
+
+    env.hosts = ['web-01', 'web-02']
+    env.user = "ubuntu"
+    env.key_filename = "/home/x230/.ssh/school"
+
+    for host in env.hosts:
+        env.host_string = host
+        deployed = do_deploy(tarball)
+    return deployed
