bfe0de8e34e5b190979fdeb90a7fe84e67e2596d
hk <kiptooharon.hk@gmail.com>
2024-05-07 05:36:29 +0300

    web deployment with fabric

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -4,63 +4,65 @@
    FULL DEPLOYMENT
    create archive and distribute to the servers
 """
-from fabric.api import local, env, run, put, runs_once
-from os import path
+from fabric.api import env, run, put, local
 from datetime import datetime
-env.hosts = ['web-01', 'web-02']
-env.user = "ubuntu"
-env.key_filename = "/home/x230/.ssh/school"
+from os import mkdir, path
+env.user = 'ubuntu'
 
 
-@runs_once
 def do_pack():
-    """
-        pack web pages to .tgz file for upload
-    """
-    now = datetime.utcnow().strftime("%Y%m%d%H%M%S").replace('/', '')
+    """ function to pack web pages for deployment"""
 
-    if path.isdir("versions") is False:
-        local("rm -f versions")
-        mkdir("versions")
-    version = "versions/web_static_" + now + ".tgz"
-    if (local("tar -cvzf {} web_static".format(version)).succeeded):
-        return version
+    now = datetime.utcnow().strftime("%Y%d%m%H%M%S")
+    file = "web_static_" + str(now) + ".tgz"
+    print("Packing web_static to {}".format(file))
+
+    if path.exists("versions") and path.isdir("versions"):
+        if (local("tar -cvzf versions/{} {}".format(file, "web_static"))):
+            return file
+    else:
+        try:
+            mkdir("versions")
+            if (local("tar -cvzf versions/{} {}".format(file, "web_static"))):
+                return file
+        except Exception as e:
+            return None
     return None
 
 
 def do_deploy(archive_path):
-    """
-        deploy the archive, untar and point the server symlink
-        to latest deployed version
-    """
+    """ deploy arhhcive file to servers /tmp"""
 
-    if path.exists(archive_path):
-        remote = "/tmp/{}".format(archive_path.split('/')[1])
-        put(remote_path="/tmp/", local_path=archive_path)
-        release_dir = "/data/web_static/releases/" + \
-            archive_path.split('.')[0].split('/')[1] + "/"
-        run("mkdir -p {}".format(release_dir))
-        run("tar -xzf {} -C {}".format(remote, release_dir))
-        run("rm -f {}".format(remote))
-        run("rm -rf /data/web_static/current")
-        run("mv {}web_static/* {}".format(release_dir, release_dir))
-        run("rm -rf {}web_static".format(release_dir))
-        run("ln -s {} /data/web_static/current".format(release_dir))
-        print("New version deployed!")
-        return True
-    return False
-
-
-@runs_once
-def deploy():
-    """ full deployment"""
-
-    tarball = do_pack()
-    if (tarball is None):
+    if path.exists(archive_path) is False:
         return False
 
+    if (put(archive_path, "/tmp").failed):
+        return False
+    else:
+        version = archive_path.split('/')[-1].split(".")[0]
+        f_loc = '/tmp/' + version + '.tgz'
+        f_dest = '/data/web_static/releases/{}/'.format(version)
+        if (run("mkdir -p {}".format(f_dest)).failed):
+            return False
+        run("tar -xzf {} -C {}".format(f_loc, f_dest))
+        run("rm {}".format(f_loc))
+        run("mv {}web_static/* {}".format(f_dest, f_dest))
+        run("rm -rf {}web_static".format(f_dest))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(f_dest))
+        print("New version deployed!")
+    return True
+
+
+def deploy():
+    """ full deployment create archive and deploy to servers"""
+
+    file_path = do_pack()
+    if file_path is None:
+        return False
+    env.hosts = ['54.173.110.95', '34.207.156.58']
     for host in env.hosts:
         env.host_string = host
-        deployed = do_deploy(tarball)
-
-    return deployed
+        if not do_deploy(file_path):
+            return False
+    return True
