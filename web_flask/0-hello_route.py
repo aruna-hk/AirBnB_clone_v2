917f7bf7ff3b44d749c1ceee736ddfa40c8dda72
hk <kiptooharon.hk@gmail.com>
2024-06-13 14:15:10 +0300

    flask app

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
 """ flask webapp """
+from flask import Flask
+app = Flask(__name__)
 
 
+@app.route("/", strict_slashes=False)
 def hello():
     """ root directory serving"""
     return "Hello HBNB"
@@ -10,7 +13,5 @@ def hello():
 
 if __name__ == "__main__":
     """ run the app"""
-    from flask import Flask
-    app = Flask(__name__)
-    app.add_url_rule("/", "hello", hello, strict_slashes=False)
+
     app.run(host="0.0.0.0", port=5000)
