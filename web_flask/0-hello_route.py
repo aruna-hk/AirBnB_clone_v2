6c2a75b3b51cf0a3914e0d46d283e9e1e37aed33
hk <kiptooharon.hk@gmail.com>
2024-06-13 14:19:54 +0300

    output and add_url_rule update

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 
 """ flask webapp """
-from flask import Flask
-app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
 def hello():
     """ root directory serving"""
-    return "Hello HBNB"
+
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
     """ run the app"""
-
+    from flask import Flask
+    app = Flask(__name__)
+    app.add_url_rule("/", "hello", hello, strict_slashes=False)
     app.run(host="0.0.0.0", port=5000)
